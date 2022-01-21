e68c8a7d09d6adad8b4fbfb51b45800d6ea0f61f
Gazook89 <58999374+Gazook89@users.noreply.github.com>
2022-01-20 20:54:17 -0600

    get token list setup to receive tokens

--- import.js
+++ import.js
@@ -4,6 +4,9 @@ export function importJSON(evt) {
     reader.readAsText(jsonFile);
     reader.onload = function() {
         const arr = JSON.parse(reader.result).rules;
+        const obj = {
+
+        }
         localStorage.setItem('full-set', JSON.stringify(arr));
         localStorage.setItem('imported-file-name', jsonFile.name);
     };
@@ -11,4 +14,7 @@ export function importJSON(evt) {
         console.log((reader.error));
     };
 };
+
+//  newCard creates from full-set, which is an object, with each property key being equal to a file name, and the matching property value being an array of squares.
+//  the defaultSquares should also act just like an imported file, which appears in the file list and can be removed (and show no squares if no files involved), or added to with imports.
 
